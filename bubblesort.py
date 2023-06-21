2e02eecdc31c00dc50d442307bc1cdcd9a0f9f81
sinonfan <darrenm324@gmail.com>
2023-06-21 19:11:47 -0400

    bubblesortdone

---
+++ bubblesort.py
@@ -0,0 +1,20 @@
+size = int(input("Enter size: "))
+arr = list(map(int, input("Enter elements of array: ").split()))
+swaps = 0
+
+for i in range(size):
+    swapped = False
+
+    #upper bound is size - i - 1 because on every iteration, the last item should be in its place so no need to iterate
+    for j in range(size - i - 1):
+        if arr[j] > arr[j+1]:
+            arr[j], arr[j+1] = arr[j+1], arr[j]
+            swaps += 1
+            swapped = True
+
+        if not swapped:
+            break
+
+
+print(swaps)
+
