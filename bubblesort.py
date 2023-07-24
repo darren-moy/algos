a6628ed7b6ae4c528c926a1c7e6e2d04f701e9c1
sinonfan <darrenm324@gmail.com>
2023-07-24 16:39:11 -0400

    bubblesortbulletproof

--- bubblesort.py
+++ bubblesort.py
@@ -1,3 +1,4 @@
+import sys
 def bubble_sort(arr):
     n = len(arr)
 
@@ -16,11 +17,25 @@ def bubble_sort(arr):
     return arr
 
 
+def checkinp(message):
+    inp = input(message)
+    if inp.isnumeric():
+        inp = int(inp)
+        if inp == 1 or inp == 2:
+            return inp
+        else:
+            print('The input you gave was not a choice. Please try again.')
+            sys.exit(1)
+    else:
+        print('The input you gave was not an integer. Please try again.')
+        sys.exit(1)
+
 if __name__ == "__main__":
+    orientation = checkinp("Please enter 1 if you would like the sort to be in increasing order and 2 for decreasing order: ")
+    which_sort = checkinp("Please enter 1 for sorting integers and 2 for sorting multiple strings by the first letter: ")
+
     try:
-        orientation = int(input("Please enter 1 if you would like the sort to be in increasing order and 2 for decreasing order: "))
-        choice = int(input("Please enter 1 for sorting integers and 2 for sorting multiple strings by the first letter: "))
-        if choice == 1:
+        if which_sort == 1:
             try:
                 elements = input("Please enter the elements with a space in between them: ").split()
                 inp_arr = [int(element) for element in elements]
@@ -49,7 +64,7 @@ if __name__ == "__main__":
                     print(f"The sorted integers are: {output}. Have a nice day!")
             except ValueError:
                 print("Invalid input, please try again with a valid input.")
-        elif choice == 2:
+        elif which_sort == 2:
             try:
                 elements = input("Please enter the elements with a space in between each: ").split()
                 if len(elements) <= 1:
@@ -78,5 +93,5 @@ if __name__ == "__main__":
             except ValueError:
                 print("Invalid input, please try again with a valid input.")
     except ValueError:
-        print("Invalid input, please try again with valid input.")
+        print("Invalid input, please try again with a valid input.")
 
